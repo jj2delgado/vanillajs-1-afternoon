2d81ca55f6697e4f0ddaa442b7427ec827035a3c
jj2delgado <jj2delgado@yahoo.com>
2019-06-10 16:37:52 -0600

    Finished

---
+++ Part2/index.js
@@ -0,0 +1,7 @@
+let cardID = document.getElementById('idInput')
+let cardColor = document.getElementById('colorInput')
+
+function setCard(){
+    let card = document.getElementById(idInput.value)
+    card.style.color = colorInput.value
+}
